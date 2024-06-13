ffbc2649a6c3ea6cabef9070721d9376120bbf52
Sopan Jadhav <112610266+sopan21@users.noreply.github.com>
2024-06-13 10:02:15 +0530

    Add files via upload
    HOMELY SPACE LOCATOR (HSL) is a web application designed to connect landlords and renters efficiently. Landlords can list their properties, including rooms, flats, and houses, while renters can search for accommodations in specific areas. The platform uses PHP, MySQL, HTML, CSS, and JavaScript to deliver a seamless user experience.

---
+++ about.php
@@ -0,0 +1,214 @@
+<?php  
+
+include 'components/connect.php';
+
+if(isset($_COOKIE['user_id'])){
+   $user_id = $_COOKIE['user_id'];
+}else{
+   $user_id = '';
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>About Us</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php include 'components/user_header.php'; ?>
+
+<!-- about section starts  -->
+
+<section class="about">
+
+   <div class="row">
+      <div class="image">
+         <img src="images/about-img.svg" alt="">
+      </div>
+      <div class="content">
+         <h3>why choose us?</h3>
+         <p>Our platform is your go-to destination for all your rental and real estate needs. With a wide range of rental properties and real estate listings, user-friendly interface, and comprehensive services for both renters and property owners, choosing us ensures a seamless and efficient experience in finding or listing rental properties and real estate assets.</p>
+         <a href="contact.php" class="inline-btn">contact us</a>
+      </div>
+   </div>
+
+</section>
+
+<!-- about section ends -->
+
+<!-- steps section starts  -->
+
+<section class="steps">
+
+   <h1 class="heading">3 simple steps</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <img src="images/step-1.png" alt="">
+         <h3>search property</h3>
+         <p>Find your perfect property quickly with detailed information and high-quality images.</p>
+      </div>
+
+      <div class="box">
+         <img src="images/step-2.png" alt="">
+         <h3>contact agents</h3>
+         <p>Connect with experienced agents directly through our platform to get personalized assistance and guidance.</p>
+      </div>
+
+      <div class="box">
+         <img src="images/step-3.png" alt="">
+         <h3>enjoy property</h3>
+         <p>Discover your ideal rental with our intuitive platform. Start enjoying your new property journey today!</p>
+      </div>
+
+   </div>
+
+</section>
+
+<!-- steps section ends -->
+
+<!-- review section starts  -->
+
+<section class="reviews">
+
+   <h1 class="heading">client's reviews</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <div class="user">
+            <img src="images/pic-1.png" alt="">
+            <div>
+               <h3>Ram</h3>
+               <div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star-half-alt"></i>
+               </div>
+            </div>
+         </div>
+         <p>I found my dream home quickly and effortlessly on this platform. The user-friendly interface and extensive listings made the process a breeze. Highly recommended!</p>
+      </div>
+
+      <div class="box">
+         <div class="user">
+            <img src="images/pic-2.png" alt="">
+            <div>
+               <h3>Ananya</h3>
+               <div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star-half-alt"></i>
+               </div>
+            </div>
+         </div>
+         <p>Selling my house was stress-free thanks to this platform. Professional service, wide reach, 24/7 customer care support, and a fantastic outcome.Highly recommended!</p>
+      </div>
+
+      <div class="box">
+         <div class="user">
+            <img src="images/pic-3.png" alt="">
+            <div>
+               <h3>Soham</h3>
+               <div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star-half-alt"></i>
+               </div>
+            </div>
+         </div>
+         <p>This platform simplified my apartment search. Easy navigation, detailed listings, 24/7 customer care support, and a smooth process from start to finish.</p>
+      </div>
+
+      <div class="box">
+         <div class="user">
+            <img src="images/pic-4.png" alt="">
+            <div>
+               <h3>Ankita</h3>
+               <div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star-half-alt"></i>
+               </div>
+            </div>
+         </div>
+         <p>Found the perfect shop location in no time with this platform. Extensive options, accurate listings, and a great user experience.</p>
+      </div>
+
+      <div class="box">
+         <div class="user">
+            <img src="images/pic-5.png" alt="">
+            <div>
+               <h3>Sarthak</h3>
+               <div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star-half-alt"></i>
+               </div>
+            </div>
+         </div>
+         <p>Effortlessly found a flat in a new city using this platform. Wide range of listings, intuitive search, and hassle-free process.</p>
+      </div>
+
+      <div class="box">
+         <div class="user">
+            <img src="images/pic-6.png" alt="">
+            <div>
+               <h3>Anjali</h3>
+               <div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star-half-alt"></i>
+               </div>
+            </div>
+         </div>
+         <p>Highly satisfied with my home search experience on this platform. Easy browsing, efficient process, and a great outcome.</p>
+      </div>
+
+   </div>
+
+</section>
+
+<!-- review section ends -->
+
+
+
+
+
+
+
+
+
+
+<?php include 'components/footer.php'; ?>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
